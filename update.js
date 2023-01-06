162390271ded150206899936db9eedd8db6dc245
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-01-06 22:33:57 +0100

    Find all entries and update timestamp

--- update.js
+++ update.js
@@ -1,7 +1,40 @@
 const loadBauprogramm = require('./src/loadBauprogramm')
+const LokiJS = require('lokijs')
 
-loadBauprogramm('https://www.wien.gv.at/verkehr/radfahren/bauen/programm/',
-  (err, list) => {
-    console.log(JSON.stringify(list, null, '  '))
+const db = new LokiJS('data/data.db', {
+  autoload: true,
+  autoloadCallback: init,
+  autosave: true
+})
+
+function init () {
+  let programm = db.getCollection('entries')
+  if (programm === null) {
+    programm = db.addCollection('entries')
   }
-)
+
+  const ts = new Date().toISOString()
+
+  loadBauprogramm('https://www.wien.gv.at/verkehr/radfahren/bauen/programm/',
+    (err, list) => {
+      list.forEach(entry => {
+        const results = programm.find({
+          ort: { $eq: entry.ort },
+          year: { $eq: entry.year }
+        })
+
+        if (results.length) {
+          entry = results[0]
+          entry.ts = ts
+
+          programm.update(entry)
+        } else {
+          entry.ts = ts
+          entry.created = ts
+
+          programm.insert(entry)
+        }
+      })
+    }
+  )
+}
